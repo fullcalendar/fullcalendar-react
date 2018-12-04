2558f21b09f172ab231a2c107237439387b82c86
Josh Ruff <josh.ruff@sardius.media>
2018-12-04 14:16:34 -0600

    Reworked webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,45 @@
-const webpack = require('webpack');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
-  devServer: {
-    historyApiFallback: true,
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] },
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
-      { test: /\.less$/, loaders: ['style-loader', 'css-loader', 'less-loader'] },
-      { test: /\.(jpe?g|png|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/, loader: 'url-loader?limit=100000' },
-    ],
-  },
+  entry: [
+    './src/index.js'
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'index.js',
+    libraryTarget: 'umd'
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ],
   resolve: {
-    modules: [path.resolve('./src'), path.resolve('./node_modules'), path.resolve('./dist'),],
+    extensions: ['.js', '.jsx']
   },
-  plugins: [],
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        exclude: /build|node_modules|styles/,
+        loaders: ['babel-loader'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      }
+    ]
+  },
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      }
+    }
+  ]
 };
