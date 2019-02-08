d3a874081b9feff0fa9e7e4e6843104dedc97a12
Josh Ruff <missions.joshruff@gmail.com>
2019-02-08 16:29:40 -0600

    temp remove rrule

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Calendar } from 'fullcalendar';
-import 'fullcalendar/dist/plugins/rrule';
+// import 'fullcalendar/dist/plugins/rrule';
 import CalendarOptionsMapper from './calendarOptionsMapper';
 
 export default class FullCalendar extends React.Component{
