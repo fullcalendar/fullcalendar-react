f9c4204346d14f8f2e7306fa1988d527bf659fc5
Josh Ruff <missions.joshruff@gmail.com>
2019-03-25 19:46:31 -0500

    rewrote jest test

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { render } from 'react-testing-library';
+/* eslint-disable */
+import React from 'react'
+import { render } from 'react-testing-library'
 
-import FullCalendar from '../index';
+import FullCalendar from '../index'
 
 describe('rendering', () => {
   it('should render without crashing', () => {
-    const { container } = render(<FullCalendar />);
-  });
-});
+    const { getByText } = render(<FullCalendar />)
+    const el = getByText('today')
+    expect(el.classList[0]).toBe('fc-today-button')
+  })
+})
