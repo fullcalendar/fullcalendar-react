0a170f485fb9a49daaaee23f0576cc2f1de6852f
Adam Shaw <arshaw@users.noreply.github.com>
2019-03-31 02:03:00 -0500

    move babel config to real js file

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react'
+  ],
+  plugins: [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-throw-expressions'
+  ]
+}
