d40f5ec1cd7f1a1792af9cbd6ec2153518954fcc
Adam Shaw <arshaw@users.noreply.github.com>
2019-04-02 16:56:30 -0500

    fixup tests

--- src/FullCalendar.test.jsx
+++ src/FullCalendar.test.jsx
@@ -2,44 +2,50 @@ import React from 'react'
 import { render } from 'react-testing-library'
 
 import daygridPlugin from '@fullcalendar/daygrid'
-import FullCalendar from '../dist/main.esm' // weird, but most realistic testing scenario
+import FullCalendar from '../dist/main.esm' // must run build/watch beforehand
 
 import 'jest-dom/extend-expect'
 import 'react-testing-library/cleanup-after-each'
 
-describe('rendering', () => {
-  it('should render without crashing', () => {
-    const { getByText } = render(<FullCalendar plugins={[ daygridPlugin ]} />)
-    const el = getByText('today')
-    expect(el.classList[0]).toBe('fc-today-button')
-  })
+const PLUGINS = [ daygridPlugin ]
 
-  it('should unmount from the DOM', () => {
-    const { unmount, queryByText } = render(<FullCalendar plugins={[ daygridPlugin ]} />)
-    unmount()
-    const el = queryByText('today')
-    expect(el).toBeNull()
-  })
+it('should render without crashing', () => {
+  const { getByText } = render(<FullCalendar plugins={PLUGINS} />)
+  const el = getByText('today')
+  expect(el.classList[0]).toBe('fc-today-button')
 })
 
-describe('callbacks and prop changes', () => {
-  it('should accept a callback', () => {
-    let bool = false
-    const callback = () => {
-      bool = true
-    }
-    render(<FullCalendar viewSkeletonRender={callback} plugins={[ daygridPlugin ]}/>)
-    expect(bool).toBeTruthy()
-  })
+it('should unmount and destroy', () => {
+  let destroyedCalled = false
+  const destroyed = () => { destroyedCalled = true }
+  const { unmount } = render(<FullCalendar plugins={PLUGINS} _destroyed={destroyed} />)
 
-  it('should have updatable props', () => {
-    const calendarApiRef = React.createRef()
-    render(<FullCalendar ref={calendarApiRef} plugins={[ daygridPlugin ]} />)
-    let locale = calendarApiRef.current.calendar.getOption('locale')
-    expect(locale).toBe('')
+  unmount()
+  expect(destroyedCalled).toBe(true)
+})
 
-    calendarApiRef.current.calendar.setOption('locale', 'fr')
-    locale = calendarApiRef.current.calendar.getOption('locale')
-    expect(locale).toBe('fr')
-  })
+it('should have updatable props', () => {
+  const { container, rerender } = render(<FullCalendar plugins={PLUGINS} />)
+  expect(container.querySelector('.fc-sat')).toBeTruthy()
+
+  rerender(<FullCalendar weekends={false} plugins={PLUGINS} />)
+  expect(container.querySelector('.fc-sat')).toBeFalsy()
+})
+
+it('should accept a callback', () => {
+  let called = false
+  const callback = () => { called = true }
+  render(<FullCalendar viewSkeletonRender={callback} plugins={PLUGINS}/>)
+  expect(called).toBe(true)
+})
+
+it('should expose an API', function() {
+  let componentRef = React.createRef()
+  render(<FullCalendar ref={componentRef} plugins={PLUGINS} />)
+  let calendarApi = componentRef.current.getApi()
+  expect(calendarApi).toBeTruthy()
+
+  let newDate = new Date(Date.UTC(2000, 0, 1))
+  calendarApi.gotoDate(newDate)
+  expect(calendarApi.getDate().valueOf()).toBe(newDate.valueOf())
 })
