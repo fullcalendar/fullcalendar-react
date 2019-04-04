5a85981a568e1a7d783b8df4def758d4b3345cf6
Adam Shaw <arshaw@users.noreply.github.com>
2019-04-04 02:12:49 -0500

    4.0.2-beta

---
+++ dist/README.md
@@ -0,0 +1,11 @@
+
+# FullCalendar React Component
+
+This project provides an official React component for FullCalendar, with all the same setting.
+
+- [FullCalendar React Component Docs](https://fullcalendar.io/docs/react)
+- [Example Project](https://github.com/fullcalendar/fullcalendar-example-projects/tree/master/react)
+- [Example Project with TypeScript](https://github.com/fullcalendar/fullcalendar-example-projects/tree/master/react-typescript)
+- [Contributor Guide](CONTRIBUTORS.md)
+
+This project is built and maintained by [Josh Ruff](https://github.com/joshuaRuff) of [Sardius Media](http://sardius.media/) in partnership with the maintainers of FullCalendar.
---
+++ dist/main.esm.js
@@ -0,0 +1,64 @@
+/*
+FullCalendar React Component v4.0.2-beta
+Docs: https://fullcalendar.io/docs/react
+License: MIT
+*/
+import { createRef, createElement, Component } from 'react';
+import { Calendar } from '@fullcalendar/core';
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+/* global Reflect, Promise */
+
+var extendStatics = function(d, b) {
+    extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return extendStatics(d, b);
+};
+
+function __extends(d, b) {
+    extendStatics(d, b);
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+}
+
+var FullCalendar = /** @class */ (function (_super) {
+    __extends(FullCalendar, _super);
+    function FullCalendar() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.elRef = createRef();
+        return _this;
+    }
+    FullCalendar.prototype.render = function () {
+        return (createElement("div", { ref: this.elRef }));
+    };
+    FullCalendar.prototype.componentDidMount = function () {
+        this.calendar = new Calendar(this.elRef.current, this.props);
+        this.calendar.render();
+    };
+    FullCalendar.prototype.componentWillReceiveProps = function (nextProps) {
+        this.calendar.resetOptions(nextProps);
+    };
+    FullCalendar.prototype.componentWillUnmount = function () {
+        this.calendar.destroy();
+    };
+    FullCalendar.prototype.getApi = function () {
+        return this.calendar;
+    };
+    return FullCalendar;
+}(Component));
+
+export default FullCalendar;
