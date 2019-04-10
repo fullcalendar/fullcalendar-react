b6f4eb7042e53a9d2ce3be17cdf5e5732e1c2cba
Paul Bournat <paul.bournat@hotmail.fr>
2019-04-10 20:18:09 +0200

    Improve code according to remarks made in PR

--- src/FullCalendar.tsx
+++ src/FullCalendar.tsx
@@ -1,22 +1,35 @@
 import * as React from 'react'
+import { forwardRef, useEffect, useImperativeHandle, useRef, MutableRefObject } from 'react'
 import { Calendar, OptionsInput } from '@fullcalendar/core'
-
-const { forwardRef, useEffect, useImperativeHandle, useRef, useState } = React
 
 export default forwardRef((props: OptionsInput, componentRef) => {
   const elRef = useRef()
-  const [calendar, setCalendar] = useState()
+  const calRef: MutableRefObject<Calendar> = useRef()
 
-  useEffect(() => {
-    const c = new Calendar(elRef.current, props)
-    setCalendar(c)
-    c.render()
-    return () => c.destroy()
-  }, [])
+  function getApi(): Calendar {
+    return calRef.current
+  }
 
-  useEffect(() => calendar && calendar.resetOptions(props), [calendar, props])
+  function init() {
+    const cal = new Calendar(elRef.current, props)
+    calRef.current = cal
+    cal.render()
+    // Cleanup callback automatically called before unmounting
+    return () => cal.destroy()
+  }
 
-  useImperativeHandle(componentRef, () => ({ getApi: (): Calendar => calendar }), [calendar])
+  function updateOptions() {
+    calRef.current.resetOptions(props);
+  }
+
+  // Calendar initialization
+  useEffect(init, [])
+
+  // Calendar options update when props change
+  useEffect(updateOptions, [calRef, props])
+
+  // Allow Calendar API to be exposed to parent component when using ref
+  useImperativeHandle(componentRef, () => ({ getApi }), [calRef])
 
   return <div ref={elRef}></div>
 })
