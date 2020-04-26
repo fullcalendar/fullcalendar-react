9248c4dcf0966ca40afddc7c73b15f860e87f8b2
Adam Shaw <arshaw@users.noreply.github.com>
2020-04-26 18:05:00 -0400

    import order change

--- src/FullCalendar.test.jsx
+++ src/FullCalendar.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-testing-library'
 
-import daygridPlugin from '@fullcalendar/daygrid'
 import FullCalendar from '../dist/main.esm' // must run build/watch beforehand
+import daygridPlugin from '@fullcalendar/daygrid'
 
 import 'jest-dom/extend-expect'
 import 'react-testing-library/cleanup-after-each'
