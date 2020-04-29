d2a1641af930feceb6b34f78e9e3ad2d4315127e
Adam Shaw <arshaw@users.noreply.github.com>
2020-04-29 18:06:53 -0400

    strip out build setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import sourcemaps from 'rollup-plugin-sourcemaps'
-import typescript from 'rollup-plugin-typescript'
 import packageConfig from './package.json'
+
 
 let isDev
 if (!/^(development|production)$/.test(process.env.BUILD)) {
@@ -12,71 +12,38 @@ if (!/^(development|production)$/.test(process.env.BUILD)) {
   isDev = process.env.BUILD === 'development'
 }
 
-let sourcemap = isDev ? 'inline' : false
 
 const BROWSER_GLOBAL = 'FullCalendarReact'
 const EXTERNAL_BROWSER_GLOBALS = {
   'react': 'React',
   'react-dom': 'ReactDOM',
-  '@fullcalendar/core': 'FullCalendar'
+  '@fullcalendar/common': 'FullCalendar'
 }
-const ESM_EXTERNALS = [
-  // put module names here that we DON'T want to bundle in the ES build
+const OTHER_EXTERNAL = [ 'tslib' ]
+
+
+let sourcemap = isDev ? 'inline' : false
+let plugins = [
+  nodeResolve({ jail: 'src' }), // any files outside of here are considered external libs
+  commonjs() // allows importing of external cjs modules
 ]
-const OUTPUT_SETTINGS = {
-  umd: {
-    format: 'umd',
-    file: 'dist/main.umd.js',
-    exports: 'named',
-    name: BROWSER_GLOBAL,
-    globals: EXTERNAL_BROWSER_GLOBALS,
+if (isDev) {
+  plugins.push(sourcemaps())
+}
+
+
+export default {
+  input: 'tmp/tsc-output/FullCalendar.js',
+  output: {
+    format: 'es',
+    file: 'dist/main.js',
     banner: buildBanner,
     sourcemap
   },
-  esm: {
-    format: 'es',
-    file: 'dist/main.esm.js',
-    banner: buildBanner,
-    sourcemap
-  }
+  external: Object.keys(EXTERNAL_BROWSER_GLOBALS).concat(OTHER_EXTERNAL),
+  plugins
 }
 
-export default [
-  buildSettings('umd'),
-  buildSettings('esm')
-]
-
-function buildSettings(format) {
-  let external = Object.keys(EXTERNAL_BROWSER_GLOBALS)
-  let plugins = []
-
-  if (format === 'esm') {
-    external = external.concat(ESM_EXTERNALS)
-    plugins.push(
-      nodeResolve({ jail: 'src' }) // any files outside of here are considered external libs
-    )
-  } else {
-    plugins.push(
-      nodeResolve()
-    )
-  }
-
-  plugins.push(
-    commonjs(), // allows importing of external cjs modules
-    typescript() // will use tsconfig.json
-  )
-
-  if (isDev) {
-    plugins.push(sourcemaps())
-  }
-
-  return {
-    input: 'src/FullCalendar.tsx',
-    output: OUTPUT_SETTINGS[format],
-    external,
-    plugins
-  }
-}
 
 function buildBanner() {
   return '/*\n' +
