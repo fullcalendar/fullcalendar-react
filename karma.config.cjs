feadab479a09e865ddb858fb879da873543b36d5
Adam Shaw <arshaw@users.noreply.github.com>
2020-10-29 17:21:46 -0400

    explicitly register karma plugins

--- karma.config.cjs
+++ karma.config.cjs
@@ -2,6 +2,12 @@
 module.exports = function(config) {
   config.set({
     basePath: '.',
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-sourcemap-loader'),
+      require('karma-spec-reporter'),
+      require('karma-chrome-launcher'),
+    ],
     frameworks: [ 'jasmine' ],
     files: [
       'tmp/tests.js',
